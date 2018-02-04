5f20f7af49cdf900e47e78dae924a3d1fdc74270
aakame <hkaloni@gmail.com>
2018-02-04 20:30:35 +0530

    Wrong Changes Fixed bugs removed

--- app/src/main/java/com/example/acer/goldenhour/DriverMapActivity.java
+++ app/src/main/java/com/example/acer/goldenhour/DriverMapActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -146,6 +147,9 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+                DatabaseReference diverLogout = FirebaseDatabase.getInstance().getReference().child("LoggedIn").child(deviceId);
+                diverLogout.removeValue();
                 isLoggingOut = true;
                 disconnectDriver();
                 FirebaseAuth.getInstance().signOut();
